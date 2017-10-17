ae7429013eb57a06bfa47ce99523e6d55c84f3f9
Nathan <nprisbrey@users.noreply.github.com>
2017-10-17 09:33:23 -0700

    Added "printf" Line + Change File Name
    I added a printf line because it seemed like it could be appropriate. :) I also renamed the class and the class file because printf is a command as well; probably shouldn't get the class and the command confused while coding (just good practice). :)

--- printfclass.java
+++ printfclass.java
@@ -1,8 +1,9 @@
-public class printf{
+public class printfclass {
 	public static void main(String[] args) { 
 		System.out.println(22);
 		System.out.println(36.5);
 		System.out.format("my number is %s %n", "35.5");
+		System.out.printf("My number is %d",35.5); //Implementation of printf
 		System.out.println("A");
 	}
 }
