7cae0d9a2430b25a04461cdfd197f92974e52e60
isaaclloyd <32470268+isaaclloyd@users.noreply.github.com>
2017-10-16 19:13:46 -0700

    adding homework 10-16-17

---
+++ printf.java
@@ -0,0 +1,8 @@
+public class printf{
+	public static void main(String[] args) { 
+		System.out.println(22);
+		System.out.println(36.5);
+		System.out.format("my number is %s %n", "35.5");
+		System.out.println("A");
+	}
+}
